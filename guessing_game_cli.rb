1b18f26561e298d16617e17f83a56ab8402b54dd
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-23 18:00:16 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,10 +4,10 @@ def run_guessing_game
   input = gets.chomp
   if input.to_i == num
     puts "You guessed the correct number!"
-  elsif input.to_i != num
-    puts "Sorry! The computer guessed #{num}."
-  else
+  elsif input == 'exit'
     puts "Goodbye!"
+  else
+    puts "Sorry! The computer guessed #{num}."
   end
 end
 
